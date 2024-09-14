8504c228c2987766cf7fcaecb64708a66cd433cd
thrkrif <103479629+thrkrif@users.noreply.github.com>
2024-09-14 20:28:09 +0900

    24/09/14

---
+++ src/generic/ex1/IntegerBox.java
@@ -0,0 +1,13 @@
+package generic.ex1;
+
+public class IntegerBox {
+    private Integer value;
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
